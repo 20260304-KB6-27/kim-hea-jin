66fad9e8ad19bbc6311f6d4dba0fb70353f3366e
KHJ <jin3574@dgu.ac.kr>
2026-04-29 15:45:59 +0900

    26.04.29_lambda_advanced

---
+++ 04_Java/ch11-lambda/src/advanced/ch16/sec05/exam01/MethodReferenceExample.java
@@ -0,0 +1,18 @@
+package advanced.ch16.sec05.exam01;
+
+public class MethodReferenceExample {
+  public static void main(String[] args) {
+    Person person = new Person();
+
+    // 람다식
+    // person.action((x, y) -> Computer.staticMethod(x, y));
+    // static method 참조
+    person.action(Computer :: staticMethod);
+
+    Computer com = new Computer();
+    // 람다식
+    // person.action((x, y) -> com.instanceMethod(x, y));
+    // instance method 참조
+    person.action(com :: instanceMethod);
+  }
+}
