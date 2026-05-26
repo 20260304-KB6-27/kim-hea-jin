f1c64c67d0d40fe820dd8c84f1ccdc54a6213dd2
KHJ <jin3574@dgu.ac.kr>
2026-05-26 09:26:14 +0900

    260526_sql기본_task
    심화 실습 나머지

--- 05_DATABASE/02_SQL_기본/task_advanced/03_employees.sql
+++ 05_DATABASE/02_SQL_기본/task_advanced/03_employees.sql
@@ -14,6 +14,36 @@ where emp_no = (select emp_no
                 where to_date = '9999-01-01'
                     and dept_no = 'd005');
 
+-- 입사일 내림차순 정렬, 한 페이지당 20명 출력
 select *
 from employees
-order by hire_date desc;
+order by hire_date desc
+limit 20 offset 0;
+
+-- 재직자 총 수
+select count(*) as `재직자 총 수`
+from dept_manager
+where to_date = '9999-01-01';
+
+-- 재직자 평균 급여
+select avg(salary) as `평균 급여`
+from salaries
+where to_date = '9999-01-01';
+
+-- 재직자 평균 급여보다 더 많이 받는 재직자
+select *
+from salaries
+where to_date = '9999-01-01'
+    and salary > (
+        select avg(salary)
+        from salaries
+        where to_date = '9999-01-01'
+    );
+
+-- 각 부서별 재직자 수
+-- 부서번호 오름차순 정렬
+select dept_no, count(*) as `부서별 재직자 수`
+from dept_emp
+where to_date = '9999-01-01'
+group by dept_no
+order by dept_no;
