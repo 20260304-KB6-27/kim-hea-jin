c8db248d43a6661df2b5035f50b62e5529e54769
KHJ <jin3574@dgu.ac.kr>
2026-07-03 15:38:23 +0900

    260703_loginlogout_lecture

--- 08_Spring_Vue/backend/src/main/java/org/scoula/security/config/SecurityConfig.java
+++ 08_Spring_Vue/backend/src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -42,119 +42,120 @@ import java.util.Arrays;
 @ComponentScan(basePackages = {"org.scoula.security"})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter; // JWT 인증필터
-    private final AuthenticationErrorFilter authenticationErrorFilter; // jwt 예외 필터
-    private final CustomAuthenticationEntryPoint authEntryPoint; // 미인증 요청 처리
-    private final CustomAccessDeniedHandler customAccessDeniedHandler; // 미인가 처리
-    @Autowired
-    private JwtUsernamePasswordAuthenticationFilter jwtUsernamePasswordAuthenticationFilter;
+  private final UserDetailsService userDetailsService;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter; // JWT 인증필터
+  private final AuthenticationErrorFilter authenticationErrorFilter; // jwt 예외 필터
+  private final CustomAuthenticationEntryPoint authEntryPoint; // 미인증 요청 처리
+  private final CustomAccessDeniedHandler customAccessDeniedHandler; // 미인가 처리
+  @Autowired
+  private JwtUsernamePasswordAuthenticationFilter jwtUsernamePasswordAuthenticationFilter;
 
-    // 문자셋필터
-    public CharacterEncodingFilter encodingFilter() {
-        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
-        encodingFilter.setEncoding("UTF-8");
-        encodingFilter.setForceEncoding(true);
-        return encodingFilter;
-    }
+  // 문자셋필터
+  public CharacterEncodingFilter encodingFilter() {
+    CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+    encodingFilter.setEncoding("UTF-8");
+    encodingFilter.setForceEncoding(true);
+    return encodingFilter;
+  }
 
-    /*
-     * CSRF : 로그인한 사용자를 악의적인 사이트에서 몰래 요청을 보내게하는 공격
-     * */
-    // 시큐리티 세팅할 공간
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // CSRF 필터 앞에다 encodingFilter를 놓겠다.
-        http.addFilterBefore(encodingFilter(), CsrfFilter.class)
-                // JWT 예외필터 -> JWT 인증필터 -> 로그인 필터 -> Usernamepassword필터
-                .addFilterBefore(authenticationErrorFilter,
-                        UsernamePasswordAuthenticationFilter.class) // JWT 예외 필터
-                .addFilterBefore(jwtAuthenticationFilter,
-                        UsernamePasswordAuthenticationFilter.class) // JWT 인증 필터
-                .addFilterBefore(jwtUsernamePasswordAuthenticationFilter,
-                        UsernamePasswordAuthenticationFilter.class); // 로그인 필터
+  /*
+   * CSRF : 로그인한 사용자를 악의적인 사이트에서 몰래 요청을 보내게하는 공격
+   * */
+  // 시큐리티 세팅할 공간
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // CSRF 필터 앞에다 encodingFilter를 놓겠다.
+    http.addFilterBefore(encodingFilter(), CsrfFilter.class)
+            // JWT 예외필터 -> JWT 인증필터 -> 로그인 필터 -> Usernamepassword필터
+            .addFilterBefore(authenticationErrorFilter,
+                    UsernamePasswordAuthenticationFilter.class) // JWT 예외 필터
+            .addFilterBefore(jwtAuthenticationFilter,
+                    UsernamePasswordAuthenticationFilter.class) // JWT 인증 필터
+            .addFilterBefore(jwtUsernamePasswordAuthenticationFilter,
+                    UsernamePasswordAuthenticationFilter.class); // 로그인 필터
 
-        // 예외 핸들러 등록
-        http.exceptionHandling()
-                .authenticationEntryPoint(authEntryPoint)// 인증이 안된 경우의 핸들러
-                .accessDeniedHandler(customAccessDeniedHandler); // 인가 문제 핸들러
+    // 예외 핸들러 등록
+    http.exceptionHandling()
+            .authenticationEntryPoint(authEntryPoint)// 인증이 안된 경우의 핸들러
+            .accessDeniedHandler(customAccessDeniedHandler); // 인가 문제 핸들러
 
-        // CORS 설정 추가
-        http.cors();
+    // CORS 설정 추가
+    http.cors();
 
-        http
-                .httpBasic().disable() // 기본 HTTP 인증 비활성화
-                .csrf().disable() // csrf 비활성화
-                .formLogin().disable() // formlogin 비활성화
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션 생성 안함
+    http
+            .httpBasic().disable() // 기본 HTTP 인증 비활성화
+            .csrf().disable() // csrf 비활성화
+            .formLogin().disable() // formlogin 비활성화
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션 생성 안함
 
-        // URL별 접근 권한 설정
-        http.authorizeRequests()
-                .anyRequest().permitAll();
+    // URL별 접근 권한 설정
+    http.authorizeRequests()
+//          .anyRequest().permitAll();
+            .anyRequest().authenticated();
 
-    }
-
-
-    // 테스트용으로 메모리 상에 사용자정보 등록
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth
-                .userDetailsService(userDetailsService) // userDetailService
-                .passwordEncoder(passwordEncoder());
-    }
-
-    // 커스텀하기위한 Authentication 객체를 빈으로 등록
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
-
-    // 인증인가 제외 URL
-    // - 보안검사가 필요없는 정적 리소스나 특정 API는 Security Filter Chain을 거치지 않도록 설정
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers(
-                        "/assets/**",
-                        "/*", // 루트경로 바로 아래 /login, /member
-                        "/api/member/**", // /api/member 하위 경로 제외
-                        "/swagger-ui.html",
-                        "/swagger-resources/**",
-                        "/webjars/**",
-                        "/v2/api-docs"
-                );
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  }
 
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration(); // CORS 설정객체
+  // 테스트용으로 메모리 상에 사용자정보 등록
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        // origin -> 출처
+    auth
+            .userDetailsService(userDetailsService) // userDetailService
+            .passwordEncoder(passwordEncoder());
+  }
+
+  // 커스텀하기위한 Authentication 객체를 빈으로 등록
+  @Bean
+  public AuthenticationManager authenticationManager() throws Exception {
+    return super.authenticationManager();
+  }
+
+  // 인증인가 제외 URL
+  // - 보안검사가 필요없는 정적 리소스나 특정 API는 Security Filter Chain을 거치지 않도록 설정
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring()
+            .antMatchers(
+                    "/assets/**",
+                    "/*", // 루트경로 바로 아래 /login, /member
+                    "/api/member/**", // /api/member 하위 경로 제외
+                    "/swagger-ui.html",
+                    "/swagger-resources/**",
+                    "/webjars/**",
+                    "/v2/api-docs"
+            );
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration(); // CORS 설정객체
+
+    // origin -> 출처
 //        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // 모든 출처에대해 허용
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://127.0.0.1:5500"));
+    configuration.setAllowedOriginPatterns(Arrays.asList("http://127.0.0.1:5500"));
 
-        // 허용할 HTTP 메소드 목록
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+    // 허용할 HTTP 메소드 목록
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 
-        // 모든 요청헤더 허용
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+    // 모든 요청헤더 허용
+    configuration.setAllowedHeaders(Arrays.asList("*"));
 
-        // 쿠키, Authorization 헤더 등 자격증명을 포함한 요청 허용
-        configuration.setAllowCredentials(true);
+    // 쿠키, Authorization 헤더 등 자격증명을 포함한 요청 허용
+    configuration.setAllowCredentials(true);
 
-        // 특정 URL 경로 패턴에 대해 CORS 설정 적용
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+    // 특정 URL 경로 패턴에 대해 CORS 설정 적용
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
 
-        return source; // Security 필터체인에서 사용
-    }
+    return source; // Security 필터체인에서 사용
+  }
 
 }
 
--- 08_Spring_Vue/frontend/src/api/authApi.js
+++ 08_Spring_Vue/frontend/src/api/authApi.js
@@ -1,35 +1,33 @@
 // import api from 'axios';
 import api from '@/api';
 
-const BASE_URL = "/api/member";
-const headers = {"Content-Type" : "multipart/form-data"};  // 파일 업로드용 헤더
+const BASE_URL = '/api/member';
+const headers = { 'Content-Type': 'multipart/form-data' }; // 파일 업로드용 헤더
 
 export default {
+  // 중복 체크 (true/false return)
+  async checkUsername(username) {
+    const { data } = await api.get(`${BASE_URL}/checkusername/${username}`);
+    console.log(`🥹AUTH GET CHECKUSERNAME : `, data);
+    return data;
+  },
 
-    // 중복 체크 (true/false return)
-    async checkUsername(username) {
+  // 회원가입 요청 API 호출
+  async create(member) {
+    // formdata 객체로 전달
+    const formdata = new FormData();
+    formdata.append('username', member.username);
+    formdata.append('email', member.email);
+    formdata.append('password', member.password);
 
-        const { data } = await api.get(`${BASE_URL}/checkusername/${username}`)
-        console.log(`🥹AUTH GET CHECKUSERNAME : `, data);
-        return data;
-    },
+    // 아바타 파일이 있을 때만
+    if (member.avatar) {
+      formdata.append('avatar', member.avatar);
+    }
 
-    // 회원가입 요청 API 호출 
-    async create(member) {
-        // formdata 객체로 전달
-        const formdata = new FormData();
-        formdata.append("username", member.username);
-        formdata.append("email", member.email);
-        formdata.append("password", member.password);
+    const { data } = await api.post(BASE_URL, formdata, headers);
+    console.log(`AUTH POST : `, data);
 
-        // 아바타 파일이 있을 때만
-        if (member.avatar) {
-            formdata.append('avatar', member.avatar);
-        }
-
-        const { data } = await api.post(BASE_URL, formdata, headers);
-        console.log(`AUTH POST : `, data);
-
-        return data;
-    },
+    return data;
+  },
 };
